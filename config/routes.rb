9c5bfcd19c3a3f1a6582203f1e72789c6e5a326c
Michael Brooks <mikeyabrooks@gmail.com>
2017-12-01 11:22:12 -0800

    add routes to home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  root 'home#index'
+
+  get 'about', to: 'home#about'
+  # devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
